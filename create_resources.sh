89bffa3dfe1b961f16413e7393095c88134addee
Insightfultechnotes <insightfulnotes2025@gmail.com>
2025-05-05 16:37:04 +0530

    Update create_resources.sh

--- create_resources.sh
+++ create_resources.sh
@@ -1,7 +1,28 @@
-1. Create a Pub/Sub Topic:
+✅ Create a Pub/Sub Topic:
 
 	gcloud pubsub topics create temperature-topic
 
-2. Create a GCS Bucket:
+✅ Create a GCS Bucket:
 
 	gsutil mb -l us-central1 gs://<your-bucket-name>
+
+ ✅ Deploy the function:
+
+	 gcloud functions deploy store_temperature_data \
+	  --runtime python310 \
+	  --trigger-resource temperature-topic \
+	  --trigger-event google.pubsub.topic.publish \
+	  --entry-point store_temperature_data \
+	  --source . \
+	  --project your-project-id
+
+  ✅ Run it in Cloud Shell:
+  
+	pip install --user google-cloud-pubsub
+	python publisher.py
+
+  ✅ Cleanup (Optional)
+
+  	gcloud functions delete store_temperature_data
+	gcloud pubsub topics delete temperature-topic
+	gsutil rm -r gs://your-unique-bucket-name/
